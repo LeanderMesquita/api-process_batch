be283fc4c5cb2ea31122ddc3bd119ff20c242375
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-27 17:31:20 -0300

    FEAT: connection with db and batch processing

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:leanderlopes87372661@localhost:5432/flask_api_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -19,11 +19,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)  
+    email = db.Column(db.String(100), nullable=False)  
     password = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(100), nullable=False)
     sector = db.Column(db.String(100), nullable=False)
     state = db.Column(db.String(100), nullable=False)
+    
+with app.app_context():    
+    db.create_all()
+
+def generator(list):
+    for data in list:
+        yield data
+
 
 @app.route('/api/users/batch', methods=['POST'])
 def process_batch():
@@ -33,20 +41,23 @@ def process_batch():
         return jsonify({"error": "Payload inválido ou não encontrado"}), 400
 
     payload = data['payload']
-
     users_created = []
     errors = []
+    
+    
+    
 
-    for user_data in payload:
+    for user_data in generator(payload):
         try:
             user_info = user_data.get("body")
+            
             if not user_info:
                 errors.append({"error": "Dados do usuário faltando", "data": user_data})
                 continue
 
             new_user = User(
                 name=user_info.get('name'),
-                last_name=user_info.get('last_name'),
+                last_name=user_info.get('lastName'),
                 email=user_info.get('email'),
                 password=user_info.get('password'),
                 role=user_info.get('role'),
@@ -71,7 +82,4 @@ def process_batch():
     }), 200
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    
     app.run(debug=True)
